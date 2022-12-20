cf9536a3c247919e62b940159690c0bf108f8891
lvke <lvke>
2022-12-20 23:13:02 +0800

    log test

--- Twinkle/src/Twinkle.h
+++ Twinkle/src/Twinkle.h
@@ -1,6 +1,8 @@
 #pragma once
 
 //For use by Twinkle applications
+#include "Twinkle/Log.h"
+
 #include "Twinkle/Application.h"
 
 #include "Twinkle/EntryPoint.h"
--- Twinkle/src/Twinkle/EntryPoint.h
+++ Twinkle/src/Twinkle/EntryPoint.h
@@ -6,6 +6,26 @@ extern Twinkle::Application* Twinkle::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Twinkle::Log::Init();
+
+	//Twinkle::Log::GetClientLogger()->trace("Client Log");
+	//Twinkle::Log::GetCoreLogger()->info("Core Log");
+	
+	LOG_CORE_DEBUG("abc{0} {0}", 1);
+	LOG_CORE_TRACE("abc");
+	LOG_CORE_INFO("abc");
+	LOG_CORE_WARN("abc");
+	LOG_CORE_ERROR("abc");
+	LOG_CORE_CRITICAL("abc");
+
+
+	LOG_APP_DEBUG("abc");
+	LOG_APP_TRACE("abc");
+	LOG_APP_INFO("abc");
+	LOG_APP_WARN("abc");
+	LOG_APP_ERROR("abc");
+	LOG_APP_CRITICAL("abc");
+
 	auto app = Twinkle::CreateApplication();
 	app->Run();
 	delete app;
